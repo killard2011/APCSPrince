6d412983d044fae8dae8db5cce428fe10b221230
killard2011 <amd41100@gmail.com>
2018-04-17 10:59:31 -0400

    Add files via upload
    This is for the AP Create Project!

---
+++ CreateProject/src/CreateProject.java
@@ -0,0 +1,17 @@
+import java.awt.*;
+import javax.swing.*;
+import java.util.*;
+
+public class CreateProject {
+
+	public static void main(String[] args) {
+		CreateProject();
+	}
+	
+	public static void CreateProject(){
+		JPanel myJPanel = new JPanel();
+		JFrame myJFrame = new JFrame();
+		myJFrame.add(myJPanel);
+		myJFrame.setVisible(true);
+	}
+}
